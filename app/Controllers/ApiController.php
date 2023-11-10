9f674387a4bb4f9eb7af95913f98977e0710bb6d
hmugambi <mugambihh@gmail.com>
2023-11-10 15:22:56 +0300

    app changes

---
+++ app/Controllers/ApiController.php
@@ -0,0 +1,10 @@
+<?php
+# defined('BASEPATH') OR exit('No direct script access allowed');
+
+namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
+use App\Models\User_model;
+
+class ApiController extends BaseController { 
+
+}
